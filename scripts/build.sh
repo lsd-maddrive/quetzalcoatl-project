bec348a18ed53e5f8582191627b5c61f184ad222
5met4nka <pavilich99@mail.ru>
2023-07-04 22:54:15 +0300

    changed the order of packages as in tigra-project

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 export CMAKE_PREFIX_PATH=/usr/local/lib/cmake/:$CMAKE_PREFIX_PATH
-export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
 
 catkin build \
     quetzalcoatl-project \
-    realsense2_camera \
-    realsense2_description \
-    cv_bridge \
-    image_geometry \
+    elp_stereo_camera \
     rtabmap \
     rtabmap_ros \
-    spatio_temporal_voxel_layer \
-    camera_calibration \
-    elp_stereo_camera \
-    stereo_image_proc \
     ublox \
+    cv_bridge \
+    stereo_image_proc \
+    camera_calibration \
+    spatio_temporal_voxel_layer \
     maddrive_urdf_tools \
     maddrive_teleop \
+    realsense2_camera \
+    realsense2_description \
+    image_geometry \
     -j$(($(nproc)-2)) \
-    --cmake-args -D OpenCV_DIR="/usr/local/lib/cmake/opencv4" -D RTABMAP_SYNC_MULTI_RGBD=ON
+    --cmake-args -D OpenCV_DIR="/usr/local/lib/cmake/opencv4" -D RTABMAP_SYNC_MULTI_RGBD=ON -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
